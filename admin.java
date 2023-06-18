6458ffb810188f44b70f6c9e8d44610962269f66
Aminul Islam Arnob <109742596+aiarnob23@users.noreply.github.com>
2023-06-18 20:36:42 +0600

    Update admin.java

--- admin.java
+++ admin.java
@@ -1,57 +1,65 @@
-
-
-import java.util.Scanner;
-
-class Bankacc {
-
-    int age, acc_no;
-    double balance;
+class Account {
+    int age;
+    int balance;
     String name;
-    Scanner scan = new Scanner(System.in);
+    int account_no;
 
-    public void newAcc() {
-        System.out.println("Enter customer full name: ");
-        name = scan.nextLine();
-        System.out.println("Enter customer age: ");
-        age = scan.nextInt();
-        System.out.println("Set a unique account number for the customer: ");
-        acc_no = scan.nextInt();
-        System.out.println("Enter customer primary balance: ");
-        balance = scan.nextDouble();
-        System.out.println("Account created successfully");
+    public void newacc() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter full name: ");
+        name = sc.nextLine();
+        System.out.println("Customer age: ");
+        age = sc.nextInt();
+        System.out.println("Set an account no: ");
+        account_no = sc.nextInt();
+        System.out.println("Primary balance of customer: ");
+        balance = sc.nextInt();
+        System.out.println("\tAccount created successfully!");
     }
 
-    public int searchAcc(int search) {
-        if (acc_no == search) {
+    public void closeacc() {
+        if (balance != 0) {
+            System.out.println("\tSorry, your account can't be deleted despite having " + balance + " taka balance.");
+        } else {
+            System.out.println("\tYour account deleted successfully!");
+            age = 0;
+            balance = 0;
+            name = "";
+            account_no = 0;
+        }
+    }
+
+    public void deposit() {
+        Scanner sc = new Scanner(System.in);
+        int amount;
+        System.out.println("Enter amount to deposit: ");
+        amount = sc.nextInt();
+        balance = balance + amount;
+        System.out.println("\tAvailable balance = " + balance);
+    }
+
+    public void withdraw() {
+        Scanner sc = new Scanner(System.in);
+        int amount;
+        System.out.println("Enter amount to withdraw: ");
+        amount = sc.nextInt();
+        if (amount <= balance) {
+            balance = balance - amount;
+            System.out.println("\tAvailable balance = " + balance);
+        } else {
+            System.out.println("\tInsufficient balance!");
+        }
+    }
+
+    public void checkbalance() {
+        System.out.println("\tYour balance is " + balance + " taka");
+    }
+
+    public int search(int k) {
+        if (k == account_no) {
             return 1;
         } else {
             return 0;
-        }
-    }
-
-    public void deposit(double amount) {
-        balance += amount;
-        System.out.println("Current Balance : " + balance);
-    }
-
-    public void withdraw(double amount) {
-        if (balance >= amount) {
-            balance -= amount;
-            System.out.println("Current Balance: " + balance);
-        } else {
-            System.out.println("Insufficient balance!");
-        }
-    }
-
-    public void closeAccount() {
-        if (balance != 0) {
-            System.out.println("This account has " + balance + " taka!");
-        } else {
-            name = "";
-            balance = 0;
-            age = 0;
-            acc_no = 0;
-            System.out.println("Account deleted successfully");
         }
     }
 }
@@ -59,126 +67,122 @@ class Bankacc {
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int c = 0;
-        Bankacc b[] = new Bankacc[10];
-        while (true) {
-            try {
+        Scanner sc = new Scanner(System.in);
+        int n = 100;
+        Account b[] = new Account[n];
+        int p = 0, acc_num, found;
+        String pass;
+        System.out.println("Enter admin pass");
+        pass = sc.next();
+        if (pass.equals("admin")) {
+            while (true) {
+                int option;
+                System.out.println("\n########## Main Menu ########");
+                System.out.println("1. Create new account");
+                System.out.println("2. Close old account");
+                System.out.println("3. Deposit money");
+                System.out.println("4. Withdraw money");
+                System.out.println("5. Check balance");
+                System.out.println("6. Exit\n");
+                System.out.print("Enter any between (1-6): ");
                 System.out.println("");
-                System.out.println("Enter your choice: ");
-                System.out.println("1: Open a new account");
-                System.out.println("2: Close an existing account");
-                System.out.println("3: Deposit Money");
-                System.out.println("4: Withdraw Money");
-                System.out.println("5: Check Balance");
-                System.out.println("6: Search Account");
-                System.out.println("7: Exit");
+                option = sc.nextInt();
 
-                int choice = scan.nextInt();
-                switch (choice) {
-                    case 1:
-                        b[c] = new Bankacc();
-                        b[c].newAcc();
-                        c++;
+                if (option == 6) {
+                    break;
+                }
+
+                switch (option) {
+                    case 1: {
+                        if (p < n) {
+                            b[p] = new Account();
+                            b[p].newacc();
+                            p++;
+                        } else {
+                            System.out.println("Maximum account limit reached!");
+                        }
                         break;
-
+                    }
                     case 2:
-                        System.out.println("Enter account no: ");
-                        int search = scan.nextInt();
+                        System.out.println("Enter account no");
+                        acc_num = sc.nextInt();
                         int flag = 0;
-                        for (int i = 0; i < c; i++) {
-                            flag = b[i].searchAcc(search);
-                            if (flag == 1) {
-                                b[i].closeAccount();
-
-                                break;
+                        for (int i = 0; i < p; i++) {
+                            if (b[i] != null) {
+                                found = b[i].search(acc_num);
+                                if (found != 0) {
+                                    b[i].closeacc();
+                                    b[i] = null; // Set the element to null to indicate account closure
+                                    flag = 1;
+                                    break;
+                                }
                             }
                         }
                         if (flag == 0) {
-                            System.out.println("Invalid Account");
+                            System.out.println("Invalid account number");
                         }
                         break;
-                    case 3:
-                        double amount;
-                        System.out.println("Enter account no: ");
-                        search = scan.nextInt();
 
-                        flag = 0;
-                        for (int i = 0; i < c; i++) {
-                            flag = b[i].searchAcc(search);
-                            if (flag == 1) {
-                                System.out.println("Enter amount");
-                                amount = scan.nextDouble();
-                                b[i].deposit(amount);
-                                break;
+                    case 3:
+                        System.out.println("Enter account no");
+                        acc_num = sc.nextInt();
+                        int depositFlag = 0;
+                        for (int i = 0; i < p; i++) {
+                            if (b[i] != null) {
+                                found = b[i].search(acc_num);
+                                if (found != 0) {
+                                    b[i].deposit();
+                                    depositFlag = 1;
+                                    break;
+                                }
                             }
                         }
-                        if (flag == 0) {
-                            System.out.println("Invalid Account");
+                        if (depositFlag == 0) {
+                            System.out.println("Invalid account number");
                         }
                         break;
 
                     case 4:
-
-                        System.out.println("Enter account no: ");
-                        search = scan.nextInt();
-
-                        flag = 0;
-                        for (int i = 0; i < c; i++) {
-                            flag = b[i].searchAcc(search);
-                            if (flag == 1) {
-                                System.out.println("Enter amount");
-                                amount = scan.nextDouble();
-                                b[i].withdraw(amount);
-                                break;
+                        System.out.println("Enter account no");
+                        acc_num = sc.nextInt();
+                        int withdrawFlag = 0;
+                        for (int i = 0; i < p; i++) {
+                            if (b[i] != null) {
+                                found = b[i].search(acc_num);
+                                if (found != 0) {
+                                    b[i].withdraw();
+                                    withdrawFlag = 1;
+                                    break;
+                                }
                             }
                         }
-                        if (flag == 0) {
-                            System.out.println("Invalid Account");
+                        if (withdrawFlag == 0) {
+                            System.out.println("Invalid account number");
                         }
                         break;
+
                     case 5:
-                        System.out.println("Enter account no: ");
-                        search = scan.nextInt();
-                        flag = 0;
-                        for (int i = 0; i < c; i++) {
-                            flag = b[i].searchAcc(search);
-                            if (flag == 1) {
-                                System.out.println("Balance : " + b[i].balance);
-                                break;
+                        System.out.println("Enter account no");
+                        acc_num = sc.nextInt();
+                        int balanceFlag = 0;
+                        for (int i = 0; i < p; i++) {
+                            if (b[i] != null) {
+                                found = b[i].search(acc_num);
+                                if (found != 0) {
+                                    b[i].checkbalance();
+                                    balanceFlag = 1;
+                                    break;
+                                }
                             }
                         }
-                        if (flag == 0) {
-                            System.out.println("Invalid Account");
+                        if (balanceFlag == 0) {
+                            System.out.println("Invalid account number");
                         }
                         break;
-                    case 6:
-                        System.out.println("Enter account no: ");
-                        search = scan.nextInt();
-                        flag = 0;
-                        for (int i = 0; i < c; i++) {
-                            if(b[i]!=null){
-                                flag = b[i].searchAcc(search);
-                            if (flag == 1) {
-                                System.out.println("Account found");
-                                System.out.println("Name: " + b[i].name);
-                                break;
-                            }
-                            }
-                        }
-                        if (flag == 0) {
-                            System.out.println("Account does not found!");
-                        }
-                        break;
-                    default:
-                        System.out.println("Invalid choice: \nTry again please.");
-                        break;
-                    case 7:
-                        return;
                 }
-            } catch (NullPointerException e) {
-                System.out.println("NullPointerException occurred: " + e.getMessage());
             }
+        } else {
+            System.out.println("Invalid password");
         }
     }
 }
